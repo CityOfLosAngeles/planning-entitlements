a0af84724325151f2216770c90ef2b16cb6bb00a
Ian Rose <ian.r.rose@gmail.com>
2020-03-05 00:57:09 +0000

    Add notebook showing TOC analysis.

--- notebooks/toc.py
+++ notebooks/toc.py
@@ -580,11 +580,11 @@ def join_with_toc_tiers(
         if other[colname].is_empty.all():
             # This branch is a workaround for a geopandas bug joining on an
             # empty geometry column, cf. GH 1315
-            current = geopandas.sjoin(current, other, how="left", op="contains",).drop(
+            current = geopandas.sjoin(current, other, how="left", op="contains").drop(
                 columns=["index_right"]
             )
         else:
-            current = geopandas.sjoin(current, other, how="left", op="within",).drop(
+            current = geopandas.sjoin(current, other, how="left", op="within").drop(
                 columns=["index_right"]
             )
     return current
