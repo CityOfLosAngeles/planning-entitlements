a359df885cffa1635ff8d8f2b37a5347e9f73267
ian-r-rose <ian.r.rose@gmail.com>
2020-06-24 13:15:32 -0700

    Simplify make_wide.

--- notebooks/pcts_census_utils.py
+++ notebooks/pcts_census_utils.py
@@ -21,40 +21,14 @@ def grab_census_table(table_name, year, main_var):
     return df
 
 
-def make_wide(df, numerator_var, denominator_var, numerator_renamed, denominator_renamed): 
-    numerator_renamed = f'{numerator_renamed}'
-    denominator_renamed = f'{denominator_renamed}'
-    
-    df = df.assign(
-        numerator = df.apply(lambda row: row.num if row.new_var==numerator_var 
-                                     else np.nan, axis=1),
-        denominator = df.apply(lambda row: row.num if row.new_var==denominator_var 
-                                       else np.nan, axis=1),
+def make_wide(df, cols):
+    return (
+        df[df.new_var.isin(cols)]
+        .assign(num=df.num.astype("Int64"))
+        .pivot(index="GEOID", columns="new_var", values="num")
+        .reset_index()
+        .rename_axis(None, axis=1)
     )
-    
-    
-    df = df.assign(
-        numerator = df.numerator.fillna(df.groupby('GEOID')['numerator'].transform('max')),
-        denominator = df.denominator.fillna(df.groupby('GEOID')['denominator'].transform('max')),
-    )
-    
-    
-    keep_col = ['GEOID', 'numerator', 'denominator']
-    
-    df = (df[keep_col].drop_duplicates()
-          .sort_values('GEOID')
-          # If the max by GEOID was still NaN, fill it in now with 0
-          .assign(
-              numerator = df.numerator.fillna(0).astype(int),
-              denominator = df.denominator.fillna(0).astype(int),
-          )
-          .rename(columns = {'numerator': numerator_renamed, 
-                               'denominator': denominator_renamed})   
-          .reset_index(drop=True)
-         )
-    
-    return df
-
 
 def aggregate_group(df, aggregate_me):
     df = (df.assign(
